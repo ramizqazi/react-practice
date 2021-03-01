c11ca08e1c5dc0993feb8ac9f54d01955dc2ac20
ramiz qazi <ramizqazi03@gmail.com>
2021-03-01 20:27:59 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Toggle from './toggle';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Toggle />,
+    document.getElementById('root')
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
